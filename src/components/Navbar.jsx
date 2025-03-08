b8a3d4cacbc299d3c6566b825346cce1f8d79f64
khafidzdev <khafidzrizziq@gmail.com>
2025-03-08 12:51:43 +0700

    u

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,33 @@
-import { useState } from 'react';
-import '../styles/Navbar.css'
+import { useState } from "react";
+import "../styles/Navbar.css";
 import { FaBars } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
-import { HashLink } from 'react-router-hash-link'
-import { Link } from 'react-router-dom'
-
+import { HashLink } from "react-router-hash-link";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    const [statusTampil, setStatusTampil] = useState('')
+    const [menuActive, setMenuActive] = useState(false);
 
-    function tampilMenu() {
-        if (statusTampil == '') {
-            setStatusTampil('tampil')
-        } else {
-            setStatusTampil('')
-        }
+    function toggleMenu() {
+        setMenuActive(!menuActive);
     }
+
     return (
-        <nav>
-            <div className="wrapper">
-                <div className="logo">
-                    <Link to="/">RumahRafif</Link>
-                </div>
-                <button onClick={tampilMenu}>
-                    {
-                        statusTampil == '' ? (<FaBars />) : (<IoMdClose />)
-                    }
-                </button>
-                <div className={`menu ${statusTampil}`} onClick={tampilMenu}>
-                    <ul>
-                        <li><HashLink to="/#portfolio">Portfolio</HashLink></li>
-                        <li><HashLink to="/#about">About</HashLink></li>
-                        <li><Link to="/experience">Experience</Link></li>
-                    </ul>
-                </div>
-            </div>
+        <nav className="navbar">
+            <h1 className="logo">
+                <Link to="/">Portfolio</Link>
+            </h1>
+            <button className={`menu-toggle ${menuActive ? "active" : ""}`} onClick={toggleMenu}>
+                {menuActive ? <IoMdClose /> : <FaBars />}
+            </button>
+            <ul className={`nav-links ${menuActive ? "active" : ""}`} onClick={toggleMenu}>
+                <li><HashLink to="/#home">Home</HashLink></li>
+                <li><HashLink to="/#about">About</HashLink></li>
+                <li><HashLink to="/#projects">Projects</HashLink></li>
+                <li><HashLink to="/#contact">Contact</HashLink></li>
+            </ul>
         </nav>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
