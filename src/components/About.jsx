95dc6f4dc72ada0e81faa1ddf24e033e94e63e5d
khafidzdev <khafidzrizziq@gmail.com>
2025-03-09 15:10:56 +0700

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import '../styles/About.css';
 import { FaPhp, FaHtml5, FaReact, FaBootstrap, FaLaravel } from "react-icons/fa";
 import { SiJavascript, SiVisualstudiocode, SiMysql, SiUnity } from "react-icons/si";
@@ -9,22 +10,34 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/autoplay';
 import { Autoplay } from 'swiper/modules';
+import GitHubCalendar from "react-github-calendar";
 
 function About() {
+    const githubUsername = "khafidzdev"; // Ganti dengan username GitHub Anda
+    const [githubProfile, setGithubProfile] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://api.github.com/users/${githubUsername}`)
+            .then(response => response.json())
+            .then(data => setGithubProfile(data))
+            .catch(error => console.error("Error fetching GitHub profile:", error));
+    }, []);
+
     return (
         <section id="about">
             <div className='wrapper'>
                 <h3>About</h3>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, alias.</p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, saepe velit, blanditiis suscipit odio non architecto ratione magnam cum porro id nesciunt repudiandae optio assumenda ab ducimus. Nesciunt, eligendi ullam!</p>
+
                 <h4>Programming Language & Tools</h4>
                 <div className="skills-carousel-wrapper">
                     <div className="skills-carousel">
                         <Swiper
-                            spaceBetween={10}
+                            spaceBetween={20}
                             slidesPerView={5}
                             loop={true}
-                            speed={3000} 
+                            speed={4000} 
                             autoplay={{
                                 delay: 0,
                                 disableOnInteraction: false,
@@ -38,13 +51,41 @@ function About() {
                             <SwiperSlide><RiTailwindCssFill color="#06B6D4" /></SwiperSlide>
                             <SwiperSlide><FaPhp color="#777BB4" /></SwiperSlide>
                             <SwiperSlide><SiVisualstudiocode color="#007ACC" /></SwiperSlide>
-                            <SwiperSlide><DiCodeigniter color="#EE4323" /></SwiperSlide>
+                            <SwiperSlide><DiCodeigniter color="#EE4623" /></SwiperSlide>
                             <SwiperSlide><FaLaravel color="#FF2D20" /></SwiperSlide>
                             <SwiperSlide><FaReact color="#61DAFB" /></SwiperSlide>
                             <SwiperSlide><SiMysql color="#4479A1" /></SwiperSlide>
-                            <SwiperSlide><SiUnity color="#000000" /></SwiperSlide>
+                            <SwiperSlide><SiUnity color="#222C37" /></SwiperSlide>
                         </Swiper>
                     </div>
+                </div>
+
+                <h4>GitHub Profile & Activity</h4>
+                {githubProfile && (
+                    <div className="github-profile">
+                        <img src={githubProfile.avatar_url} alt="GitHub Avatar" />
+                        <div className="github-info">
+                            <h3>{githubProfile.name || githubProfile.login}</h3>
+                            <p>{githubProfile.bio}</p>
+                            {/* <a href={githubProfile.html_url} target="_blank" rel="noopener noreferrer">
+                                View GitHub Profile
+                            </a> */}
+                        </div>
+                    </div>
+                )}
+
+                <div className="github-calendar-container">
+                    <GitHubCalendar 
+                        username={githubUsername}
+                        colorScheme="dark"
+                        blockSize={12}
+                        blockMargin={5}
+                        fontSize={16}
+                        fullYear={true}
+                        until="2025-12-31"
+                        showWeekdayLabels={true}
+                        hideTotalCount={true}
+                    />
                 </div>
             </div>
         </section>
