394ecaa01b25b3e3bfc311e7e9f4efd970a3ca19
khafidzdev <khafidzrizziq@gmail.com>
2025-03-11 10:56:46 +0700

    u

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,26 @@ import { FaInstagram } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
+import BlurText from "../components/BlurText/BlurText";
 import '../styles/Header.css'
 function Header() {
+    const handleAnimationComplete = () => {
+        console.log('Animation completed!');
+      };
     return (
         <header>
             <div className="header-jumbotron">
                 <img src={profilePicture} />
                 <h3>KhafidzDev</h3>
-                <p>Programmer - Content Creator - Researcher</p>
+                <BlurText
+  text="                Programmer - Content Creator - Researcher
+"
+  delay={150}
+  animateBy="words"
+  direction="top"
+  onAnimationComplete={handleAnimationComplete}
+  className="text-2xl mb-8"
+/>
                 {/* <div className='socialMedia'>
                     <a href=''><FaInstagram /></a>
                     <a href=''><FaTiktok /></a>
